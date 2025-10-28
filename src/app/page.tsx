63cf30fc31123570d282d8a02ffc547558e840bc
johnlivingprooff <johnlivingprooff@gmail.com>
2025-10-28 14:14:32 +0200

    Remove example seed file for Cloudinary images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -128,7 +128,7 @@ export default async function HomePage() {
           {featuredProperties.length > 0 ? (
             <>
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
-                {featuredProperties.map((property) => (
+                {featuredProperties.map((property: any) => (
                   <PropertyCard 
                     key={property.id}
                     id={property.id}
